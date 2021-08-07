b1f027e670b56f4850fc784546f43d9b430afeb1
Erin Quinn <equinn125@gmail.com>
2021-08-07 13:43:49 -0600

    Refactor shift methods in key and offset

--- lib/key.rb
+++ lib/key.rb
@@ -8,19 +8,26 @@ class Key
     end
   end
 
-  def split_a(key)
-    @key[0..1]
-  end
-
-  def split_b(key)
-    @key[1..2]
-  end
-
-  def split_c(key)
-    @key[2..3]
-  end
-
-  def split_d(key)
-    @key[3..4]
+  def set_shift_key
+    a_key = @key[0..1]
+    b_key = @key[1..2]
+    c_key = @key[2..3]
+    d_key = @key[3..4]
+    ["#{a_key}", "#{b_key}","#{c_key}","#{d_key}"]
   end
 end
+# def split_a(key)
+#   @key[0..1]
+# end
+#
+# def split_b(key)
+#   @key[1..2]
+# end
+#
+# def split_c(key)
+#   @key[2..3]
+# end
+#
+# def split_d(key)
+#   @key[3..4]
+# end
