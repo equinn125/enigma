1f24341aafa05579c53e2229af0762a4a1076764
Erin Quinn <equinn125@gmail.com>
2021-08-06 22:15:09 -0600

    Update initialize to have option for key generation

--- lib/key.rb
+++ lib/key.rb
@@ -1,12 +1,10 @@
 class Key
   attr_reader :key
   def initialize(key = nil)
-    @key = key
+    if key.nil?
+      @key = rand(5 ** 5).to_s.rjust(5,'0')
+    else
+      @key = key
+    end
   end
-
-  # def generate_key(key)
-  #   if key.nil?
-  #   rand(5 ** 5).to_s.rjust(5,'0')
-  #   end
-  # end
 end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -1,9 +1,16 @@
 require './lib/key'
 
 RSpec.describe Key do
-  it 'exists and' do
+  it 'exists with a pre-created key' do
     given_key = Key.new("02715")
     expect(given_key).to be_a(Key)
     expect(given_key.key).to eq("02715")
+  end
+
+  it 'exists with a random key if one is not provided' do
+    generated_key = Key.new()
+    expect(generated_key).to be_a(Key)
+    expect(generated_key.key.length).to eq(5)
+    expect(generated_key.key.class).to eq(String)
   end
 end
