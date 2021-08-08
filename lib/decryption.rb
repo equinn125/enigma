62b9793515b50ad780e2a254b2e91c178fed13f2
Erin Quinn <equinn125@gmail.com>
2021-08-07 22:36:03 -0600

    Test and impol full decryption method

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -6,4 +6,27 @@ class Decryption
     @offset= Offset.new(date)
     @alpha = ("a".."z").to_a << " "
   end
+
+  def full_shift
+    {
+      :a => @key.set_shift_key[0].to_i + @offset.set_offset_shift[0].to_i,
+      :b => @key.set_shift_key[1].to_i + @offset.set_offset_shift[1].to_i,
+      :c => @key.set_shift_key[2].to_i + @offset.set_offset_shift[2].to_i,
+      :d => @key.set_shift_key[3].to_i + @offset.set_offset_shift[3].to_i
+    }
+  end
+
+  def decrypt(message)
+  decrypted_message = []
+    message.each_char.with_index do |letter, i|
+      id = alpha.index(letter)
+      if alpha.include?(letter)
+        i_a = (id - full_shift.values[i % 4]) % alpha.length
+        decrypted_message << alpha[i_a]
+      else
+        decrypted_message << letter
+      end
+    end
+    decrypted_message.join
+  end
 end
--- spec/decryption_spec.rb
+++ spec/decryption_spec.rb
@@ -4,10 +4,29 @@ require './lib/decryption'
 
 RSpec.describe Decryption do
   it 'exists' do
-    decryption = Decryption.new("hello world", "02715", "040895")
+    decryption = Decryption.new("keder ohulw", "02715", "040895")
     expect(decryption).to be_a(Decryption)
-    expect(decryption.message).to eq("hello world")
+    expect(decryption.message).to eq("keder ohulw")
     expect(decryption.key.class).to eq(Key)
     expect(decryption.offset.class).to eq(Offset)
+  end
+
+  describe '#full_shift' do
+    it 'returns a hash with the letter as the key and the shift as the value' do
+      decryption = Decryption.new("keder ohulw", "02715", "040895")
+      expected = {
+        :a => 3,
+        :b => 27,
+        :c => 73,
+        :d => 20
+      }
+      expect(decryption.full_shift).to eq(expected)
+    end
+  end
+  describe '#decrypt(message)' do
+    it 'can return a encrypted message' do
+      decryption = Decryption.new("keder ohulw", "02715", "040895")
+      expect(decryption.decrypt("keder ohulw")).to eq("hello world")
+    end
   end
 end
