3ab912c7d1a8f97003c76304c4759fe0e67fc96e
Erin Quinn <equinn125@gmail.com>
2021-08-08 12:24:39 -0600

    sample tests and some refactoring notes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,20 +1,21 @@
 class Enigma
   def initialize
   end
+  #figure out where to handle  uppercase and additonal symbol edgecases
 
-  def encrypt(message, key = nil, date= nil)
+  def encrypt(message, key = nil, date= nil) #potentially change this to reduce redundancy
   new =  Encryption.new(message, key, date)
   {encryption: new.encrypt(message),
-    key: key,
-    date: date
+    key: Key.new(key).key,
+    date: Offset.new(date).date
   }
   end
 
   def decrypt(message, key = nil, date = nil)
     new = Decryption.new(message, key, date)
     {decryption: new.decrypt(message),
-      key: key,
-      date: date
+      key: Key.new(key).key,
+      date: Offset.new(date).date
     }
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -22,13 +22,33 @@ RSpec.describe Enigma do
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
   end
 
-  # it 'can encrypt a message using todays date' do
-  #   enigma = Enigma.new
-  #   expected = {
-  #     encryption: "keder ohulw",
-  #     key: "02715",
-  #     date: Date.today.strftime("%d%m%y")
-  #   }
-  #   expect(enigma.encrypt("hello world", "02715")).to eq(expected)
-  # end
+  it 'can encrypt a message using todays date' do #change this
+    enigma = Enigma.new
+    expected = {
+      encryption: "nefau qdxly",
+      key: "02715",
+      date: Date.today.strftime("%d%m%y")
+    }
+    expect(enigma.encrypt("hello world", "02715")).to eq(expected)
+  end
+
+  it 'can decrypt a message using todays date' do #change this test
+    enigma = Enigma.new
+    expected = {
+    decryption: "hello world",
+    key: "02715",
+    date: Date.today.strftime("%d%m%y")
+  }
+  expect(enigma.decrypt("nefau qdxly", "02715")).to eq(expected)
+  end
+
+  it 'can encrypt a message with a random key and todays date' do #change this test too
+    enigma = Enigma.new
+    expected = {
+    encryption: " ",
+    key: rand(5 ** 5).to_s.rjust(5,'0'),
+    date: Date.today.strftime("%d%m%y")
+  }
+  expect(enigma.encrypt("erin quinn")).to eq(expected)
+  end
 end
